31340bc2bac0c2338ef1b87750d27a615b8424b2
Nico-Moose <nicolasrichardson68@gmail.com>
2026-05-01 22:43:45 +0300

    Delete infoService.js

--- infoService.js
+++
@@ -1,7 +0,0 @@
-const { WIZEBOT } = require('./economyConfig');
-const { collectIncome } = require('./incomeService');
-
-module.exports = {
-  COLLECT_COOLDOWN_MS: WIZEBOT.COLLECT_COOLDOWN_MS,
-  collectFarm: collectIncome
-};
